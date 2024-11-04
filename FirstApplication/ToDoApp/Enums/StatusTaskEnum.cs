837f99d5af2cc1b9b7cba44350412146a23a5e7b
Samuel Paz <samuel.paz@sptech.school>
2024-11-04 11:15:35 -0300

    add mapping

---
+++ FirstApplication/ToDoApp/Enums/StatusTaskEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ToDoApp.Enums
+{
+    public enum StatusTaskEnum
+    {
+        [Description("Pending")]
+        Pending = 1,
+        [Description("In Progres")]
+        InProgress = 2,
+        [Description("Finished")]
+        Finished = 3
+    }
+}
